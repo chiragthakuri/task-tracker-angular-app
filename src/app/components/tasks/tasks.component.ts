840937957578fe1ac5dbc52824004fa386357938
chiragthakuri <83844592+chiragthakuri@users.noreply.github.com>
2022-07-26 11:10:48 -0500

    delete task implemented

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+  constructor(private taskservice: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskservice.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  deleteTask(task: Task) {
+    this.taskservice
+      .deleteTasks(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+}
